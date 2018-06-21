274873ccb75492aedd479749b186b504f0ba180b
easton <easton.hou@gmail.com>
2018-06-21 01:35:38 +0000

    .

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from data import Dataset, TrainFeeder, align2d, align3d
 from model import Model
+from collections import defaultdict
 import numpy as np
 import os
 import config
@@ -42,9 +43,29 @@ def run_epoch(model, feeder, optimizer, batches):
         y = tensor(qids)
         logit = model(x, y)
         sm_logit = sm(logit).transpose(1,3).transpose(2,3)
-        mask = (tensor(qids)!=config.NULL_ID).float()
+        mask = (tensor(qids)!=config.NULL_ID).float()#[batch, num_questions, ids]
+        logit_ids = logit.max(-1)[1].tolist()#[batch, num_questions, ids]
+      #  logit_counter = torch.nn.functional.softmax(logit, dim=-1).sum(-2).cpu().detach().numpy()#[batch, num_questions, sum_sm_logit]
+        logit_weight = np.ndarray(mask.shape, dtype=np.float32)
+        for idx_batch in range(mask.shape[0]):
+            for idx_question in range(mask.shape[1]):
+                id_counter = defaultdict(lambda: 0)
+                for idx_char in range(mask.shape[2]):
+                    id = logit_ids[idx_batch][idx_question][idx_char]
+                    id_counter[id] += 1
+                for idx_char in range(mask.shape[2]):
+                    id = logit_ids[idx_batch][idx_question][idx_char]
+                    tid = qids[idx_batch][idx_question][idx_char]
+                    if tid == config.NULL_ID:
+                        w = 0
+                    elif id != tid:
+                        w = id_counter[id]
+                    else:
+                        w = 1
+                    logit_weight[idx_batch, idx_question, idx_char] = w
+
         #mask[:,:,0] = 0.2
-        loss = (criterion(sm_logit, y) * mask).sum() / mask.sum()
+        loss = (criterion(sm_logit, y) * tensor(logit_weight)).sum() / mask.sum()
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()

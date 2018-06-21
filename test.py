870a05e188361dcdd130b5b6d2b289339638ba9b
eastonhou <a7031x@gmail.com>
2018-06-21 13:42:48 +0800

    .

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 import torch
+import torch.nn as nn
 
-a = torch.Tensor(5, 6).fill_(5)
+m = nn.LogSoftmax()
+loss = nn.NLLLoss(size_average=False, reduce=False)
+# input is of size N x C = 3 x 5
+input = torch.randn(3, 5, 4, requires_grad=True)
+vs = []
+for s in torch.unbind(input, 1):
+    vs.append(s)
+input = torch.cat(vs, 1).view(3, 5, 4)
+# each element in target has to have 0 <= value < C
+target = torch.tensor([[1,0,0,1],[0,0,1,0],[2,0,1,3]])
+output = loss(m(input), target)
+output.sum().backward()
 
-print('w', a)
+print('w', output)
